e566219c820bec6b3487e149119ecbe067ed39e9
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-31 15:06:18 +0200

    [Fix] fix img in js

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "female3.png"
   }
 ];
 
@@ -47,7 +47,7 @@ const createMemberCard = (member) => {
   //card effettiva
   const card =`<div class="col-12">
                 <div class="card-image">
-                    <img src="./img/${img}" alt="" >
+                    <img src="./img/${img}" alt="" class="img-fluid" >
                 </div>
                 <div class="card-text">
                     <h4>${name}</h4>
@@ -58,3 +58,20 @@ const createMemberCard = (member) => {
 
             return card
 }
+
+//arrow function per il contenuto array
+const renderTeam = (array) => {
+
+  //creo una variabile vuota per mettere insieme tutte le colonne sotto forma di stringa
+  let cards = "";
+  //creo un ciclo for per prendere il parametro cards
+  for(let i=0; i< array.length; i++) {
+    //concateno il valore di cards con quello di createMembersCard
+    cards += createMemberCard(array[i])
+  }
+  //mi recupero dal dom id team-members per assegnargli il contenuto di cards
+  document.getElementById("team-members").innerHTML = cards;
+}
+
+//chiamata funzione
+renderTeam(teamMembers);
